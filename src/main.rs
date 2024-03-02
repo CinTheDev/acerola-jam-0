4c06ef4ef1de93af7029892f09cec422c1981be1
Kevin Müller <mkevin2006@gmail.com>
2024-03-02 11:10:49 +0100

    Load test scene at startup

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,15 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
         .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(
+        SceneBundle {
+            scene: asset_server.load("test_env.glb#Scene0"),
+            ..default()
+        }
+    );
 }
