6ce4cb140cd5869773485744faa7ac881002d986
Kevin Müller <mkevin2006@gmail.com>
2024-03-02 20:59:36 +0100

    Add base for item holding

--- src/player/items.rs
+++ src/player/items.rs
@@ -1,19 +1,38 @@
 use bevy::prelude::*;
 use super::collision;
 
+static ITEM_HOLD_TRANSFORM: Transform = 
+    Transform::from_xyz(0.0, 0.0, 0.0)
+    .with_rotation(Quat::from_euler(EulerRot::YXZ, 1.0, 1.0, 0.0));
+
 pub enum ITEM_ID {
     NONE,
     SOMETHING,
 }
 
 #[derive(Bundle)]
-struct ItemBundle {
-    scene: SceneBundle,
-    collider: collision::SphereCollider,
-    item: Item,
+pub struct ItemBundle {
+    pub scene: SceneBundle,
+    pub collider: collision::SphereCollider,
+    pub item: Item,
 }
 
 #[derive(Component)]
-struct Item {
+pub struct Item {
     id: ITEM_ID,
+    pickup: bool,
+}
+
+pub fn hold_item(
+    mut q_item: Query<(&Item, &mut Transform), Without<super::Player>>,
+    q_player: Query<(&super::Player, &Transform), Without<Item>>
+) {
+    let player = q_player.single();
+    let player_properties = player.0;
+    let player_transform = player.1;
+
+    for mut item in q_item.iter_mut() {
+        let id = item.0;
+        let trans = item.1.as_mut();
+    }
 }
