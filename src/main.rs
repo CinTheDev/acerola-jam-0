91c7f8801d4fd2c563eb176a4f545e8eb180cc81
Kevin Müller <mkevin2006@gmail.com>
2024-03-02 11:34:56 +0100

    Downgrade to bevy 12.1 because lights don't work for some reason

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,14 @@ fn main() {
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(SceneBundle {
-        scene: asset_server.load("test_env.glb#Scene0"),
-        ..default()
-    });
-
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1500.0,
-            shadows_enabled: true,
-            ..default()
-        },
-        transform: Transform::from_xyz(0.0, 5.0, 0.0),
-        ..default()
-    });
-
     commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(15.0, 5.0, 0.0).looking_at(Vec3::new(0.0, 1.0, 0.0), Vec3::Y),
+        ..default()
+    });
+
+    commands.spawn(SceneBundle {
+        scene: asset_server.load("test_env.glb#Scene0"),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
         ..default()
     });
 }
