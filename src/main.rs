a008acb0b71034c46f2eb318b746ed31407686f0
Kevin Müller <mkevin2006@gmail.com>
2024-03-02 14:10:17 +0100

    Lock cursor in window

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use bevy::prelude::*;
+use bevy::window::{CursorGrabMode, PrimaryWindow};
 
 mod player;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
+        .add_systems(Startup, (setup, cursor_grab))
         .add_systems(Update, player::move_player)
         .run();
 }
@@ -18,4 +19,11 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     });
 
     player::instance_player(commands);
+}
+
+fn cursor_grab(mut query: Query<&mut Window, With<PrimaryWindow>>) {
+    let mut primary_window = query.single_mut();
+
+    primary_window.cursor.grab_mode = CursorGrabMode::Locked;
+    primary_window.cursor.visible = false;
 }
