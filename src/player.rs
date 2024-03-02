3836d5127a8451ab098f9d882499de49993c08ca
Kevin Müller <mkevin2006@gmail.com>
2024-03-02 13:49:33 +0100

    Rotate camera with mouse

--- src/player.rs
+++ src/player.rs
@@ -1,10 +1,15 @@
 use bevy::{input::mouse::MouseMotion, prelude::*};
 
+use std::f32::consts::PI;
+const PLAYER_MAX_ROTATION: f32 = PI;
+const PLAYER_MIN_ROTATION: f32 = -PI;
+
 pub fn instance_player(mut commands: Commands) {
     commands.spawn(PlayerBundle {
         player: Player {
             speed: 3.0,
-            sensitivity: 0.1,
+            sensitivity: 0.001,
+            rotation: Vec2::ZERO,
         },
         camera: Camera3dBundle {
             transform: Transform::from_xyz(0.0, 1.0, 0.0),
@@ -23,28 +28,35 @@ struct PlayerBundle {
 pub struct Player {
     speed: f32,
     sensitivity: f32,
+
+    rotation: Vec2,
 }
 
 pub fn move_player(
     time: Res<Time>,
     keyboard_input: Res<Input<KeyCode>>,
     mut mouse_input: EventReader<MouseMotion>,
-    mut query: Query<(&Player, &mut Transform)>
+    mut query: Query<(&mut Player, &mut Transform)>
 ) {
     let mut p = query.single_mut();
-    let properties = p.0;
+    let properties = p.0.as_mut();
     let transform = p.1.as_mut();
 
     let dir = get_keyboard_input(&keyboard_input, &transform);
     let mouse_delta = get_mouse_input(&mut mouse_input);
 
     let vec_move = dir * properties.speed * time.delta_seconds();
-    let mouse_rotation = mouse_delta * properties.sensitivity * time.delta_seconds();
+    let trans_rot = mouse_delta * properties.sensitivity;
 
     transform.translation += vec_move;
 
-    transform.rotate_y(mouse_rotation.x);
-    transform.rotate_local_x(mouse_rotation.y * -1.0);
+    properties.rotation += trans_rot;
+    properties.rotation.y = properties.rotation.y
+        .min(PLAYER_MAX_ROTATION)
+        .max(PLAYER_MIN_ROTATION);
+
+    transform.rotation = Quat::from_axis_angle(Vec3::Y, properties.rotation.x);
+    transform.rotation *= Quat::from_axis_angle(transform.local_x(), properties.rotation.y);
 }
 
 fn get_keyboard_input(input: &Res<Input<KeyCode>>, player_trans: &Transform) -> Vec3 {
