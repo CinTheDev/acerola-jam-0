051c0ad32598d724a0f898647eef779f7a950b40
Kevin Müller <mkevin2006@gmail.com>
2024-03-02 14:49:22 +0100

    Add base for collide and slide algorithm

--- src/player/collision.rs
+++ src/player/collision.rs
@@ -10,3 +10,16 @@ pub struct SphereCollider {
     pub position: Vec3,
     pub radius: f32,
 }
+
+pub fn check_collision_dynamic(
+    sphere: SphereCollider,
+    sphere_trans: Vec3,
+    cube: BoxCollider,
+    cube_trans: Vec3,
+    velocity: Vec3
+) -> Vec3 {
+    // Use "Collide and Slide" algorithm
+    // TODO
+
+    return velocity;
+}
