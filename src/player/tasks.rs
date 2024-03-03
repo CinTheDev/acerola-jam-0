09c2bdb82f8bf0637ed281e863079f7d0429ee85
Kevin Müller <mkevin2006@gmail.com>
2024-03-03 13:24:36 +0100

    Do some test implementation for tasks

--- src/player/tasks.rs
+++ src/player/tasks.rs
@@ -1,20 +1,32 @@
 // Structure and general implementation of tasks
 use bevy::prelude::*;
 
+const TASK_COUNT: usize = 1;
+
 pub mod test_task;
 
 #[derive(Component)]
 pub struct TaskManager {
     task_index: usize,
-    tasks: [Task],
+    tasks: [Box<dyn Task + Send + Sync>; TASK_COUNT],
 }
 
 trait Task {
     // Task has 3 behaviours: start task, do (update) task, finish task
     // These must be implemented per task
     fn start_task(&self);
-    fn check_task(&self);
+    fn check_task(&mut self);
     fn finish_task(&self);
 
     // If task finishes, next task will be started
+}
+
+pub fn instance_tasks() -> TaskManager {
+    let test_task = test_task::test_task::new(2);
+    let test_task_box = Box::new(test_task);
+
+    TaskManager {
+        task_index: 0,
+        tasks: [test_task_box],
+    }
 }
--- src/player/tasks/test_task.rs
+++ src/player/tasks/test_task.rs
@@ -1,1 +1,30 @@
 // Task specific implementations
+
+use bevy::prelude::*;
+use super::Task;
+
+pub struct test_task {
+    some_val: i32,
+}
+
+impl test_task {
+    pub fn new(val: i32) -> Self {
+        Self {
+            some_val: val,
+        }
+    }
+}
+
+impl Task for test_task {
+    fn start_task(&self) {
+        info!("Starting task: {}", self.some_val);
+    }
+
+    fn check_task(&mut self) {
+        self.some_val += 1;
+    }
+
+    fn finish_task(&self) {
+        info!("Finished task: {}", self.some_val);
+    }
+}
