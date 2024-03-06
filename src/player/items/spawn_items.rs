16da101a7ecb13b2fdd9ff109c08fd652a561351
Kevin Müller <mkevin2006@gmail.com>
2024-03-06 19:41:37 +0100

    Properly place dark matter in world

--- src/player/items/spawn_items.rs
+++ src/player/items/spawn_items.rs
@@ -6,9 +6,13 @@ pub fn spawn_all_items(commands: &mut Commands, asset_server: Res<AssetServer>) 
 }
 
 fn spawn_dark_matter(commands: &mut Commands, asset_server: Res<AssetServer>) {
+    let transform = Transform::from_xyz(-1.97848, 1.02251, 0.2582)
+    .with_rotation(Quat::from_rotation_y(265.91_f32.to_radians()));
+
     commands.spawn(ItemBundle {
         scene: SceneBundle {
             scene: asset_server.load("items/dark_matter.glb#Scene0"),
+            transform,
             ..default()
         },
         collider: SphereCollider {
@@ -17,7 +21,7 @@ fn spawn_dark_matter(commands: &mut Commands, asset_server: Res<AssetServer>) {
         item: Item {
             id: ItemId::DarkMatter,
             pickup: true,
-            desired_transform: Transform::IDENTITY,
+            desired_transform: transform,
             lerp_active: false,
         }
     });
