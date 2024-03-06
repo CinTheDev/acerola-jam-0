2685925aec4643a46717c79e1e2315d7bfb28734
Kevin Müller <mkevin2006@gmail.com>
2024-03-06 21:56:12 +0100

    Make item spawning more generic

--- src/player/items/spawn_items.rs
+++ src/player/items/spawn_items.rs
@@ -9,20 +9,38 @@ fn spawn_dark_matter(commands: &mut Commands, asset_server: Res<AssetServer>) {
     let transform = Transform::from_xyz(-1.97848, 1.02251, 0.2582)
     .with_rotation(Quat::from_rotation_y(265.91_f32.to_radians()));
 
+    spawn_item(
+        commands,
+        asset_server,
+        "items/dark_matter.glb#Scene0".to_owned(),
+        transform,
+        0.25,
+        ItemId::DarkMatter
+    )
+}
+
+fn spawn_item(
+    commands: &mut Commands,
+    asset_server: Res<AssetServer>,
+    asset_path: String,
+    transform: Transform,
+    radius: f32,
+    id: ItemId,
+) {
     commands.spawn(ItemBundle {
         scene: SceneBundle {
-            scene: asset_server.load("items/dark_matter.glb#Scene0"),
+            scene: asset_server.load(asset_path),
             transform,
             ..default()
         },
         collider: SphereCollider {
-            radius: 0.25,
+            radius,
         },
         item: Item {
-            id: ItemId::DarkMatter,
+            id,
             pickup: true,
             desired_transform: transform,
-            lerp_active: false,
+            lerp_active: true,
         }
     });
 }
