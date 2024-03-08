97194371d06c5e159df7b6ddaa4699911fd17b52
Kevin Müller <mkevin2006@gmail.com>
2024-03-08 19:33:45 +0100

    Add RotateButton

--- src/player/tasks/particle_accelerator/rotate_button.rs
+++ src/player/tasks/particle_accelerator/rotate_button.rs
@@ -1,5 +1,19 @@
 use bevy::prelude::*;
 
+use crate::player::collision::SphereCollider;
+
+#[derive(Bundle)]
+pub struct RotateButtonBundle {
+    scene: SceneBundle,
+    collider: SphereCollider,
+    rotate_button: RotateButton,
+}
+
+#[derive(Component)]
+pub struct RotateButton {
+    rotation: u8,
+}
+
 pub fn spawn_buttons(mut commands: Commands, asset_server: Res<AssetServer>) {
     let first_transform = Transform::from_xyz(-0.609, 0.85, -8.736)
         .with_rotation(Quat::from_rotation_x(28.252_f32.to_radians()));
