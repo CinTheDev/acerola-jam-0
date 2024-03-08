328949ea39bc277db19689119e4eb8a64f5c9d66
Kevin Müller <mkevin2006@gmail.com>
2024-03-08 20:23:25 +0100

    Rotate buttons using system

--- src/player/tasks/particle_accelerator/rotate_button.rs
+++ src/player/tasks/particle_accelerator/rotate_button.rs
@@ -1,6 +1,11 @@
+use std::f32::consts::PI;
+
 use bevy::prelude::*;
 
 use crate::player::{collision::{raycast_mut, SphereCollider}, Player};
+
+const BUTTON_ROTATION: f32 = 28.252 * (PI / 180.0);
+const LERP_FACTOR: f32 = 0.25;
 
 #[derive(Bundle)]
 pub struct RotateButtonBundle {
@@ -14,9 +19,9 @@ pub struct RotateButton {
     rotation: u8,
 }
 
-pub fn rotate_buttons(
+pub fn check_button_interaction(
     mut q_buttons: Query<(&Transform, &SphereCollider, &mut RotateButton)>,
-    q_player: Query<(&Transform), With<Player>>
+    q_player: Query<&Transform, With<Player>>
 ) {
     let player = q_player.single();
 
@@ -32,9 +37,27 @@ pub fn rotate_buttons(
     button.rotation = (button.rotation + 1) % 4;
 }
 
+pub fn rotate_buttons(
+    mut q_buttons: Query<(&mut Transform, &RotateButton)>
+) {
+    for button in q_buttons.iter_mut() {
+        let mut button_trans = button.0;
+        let button_prop = button.1;
+
+        let button_standard_rotation = Quat::from_rotation_x(BUTTON_ROTATION);
+        let button_desired_rotation = Quat::from_axis_angle(
+            button_standard_rotation.xyz(),
+            0.5 * PI * button_prop.rotation as f32
+        );
+        let button_total_rotation = button_standard_rotation * button_desired_rotation;
+
+        button_trans.rotation = button_trans.rotation.slerp(button_total_rotation, LERP_FACTOR);
+    }
+}
+
 pub fn spawn_buttons(mut commands: Commands, asset_server: Res<AssetServer>) {
     let first_transform = Transform::from_xyz(-0.609, 0.85, -8.736)
-        .with_rotation(Quat::from_rotation_x(28.252_f32.to_radians()));
+        .with_rotation(Quat::from_rotation_x(BUTTON_ROTATION));
 
     let dist_right: f32 = 0.228 * 1.14;
     let dist_down: f32 = 0.228 * 1.15;
