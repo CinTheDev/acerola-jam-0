2ef9dc7849015431ea578dcead31fe3086a1f30a
Kevin Müller <mkevin2006@gmail.com>
2024-03-08 15:54:05 +0100

    Write transform of first button

--- src/player/tasks/particle_accelerator/rotate_button.rs
+++ src/player/tasks/particle_accelerator/rotate_button.rs
@@ -1,3 +1,8 @@
 use bevy::prelude::*;
 
+pub fn spawn_buttons(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let first_transform = Transform::from_xyz(-0.609, 0.85, -8.736)
+        .with_rotation(Quat::from_rotation_x(28.252_f32.to_radians()));
 
+    // TODO: Spawn 14 Buttons to right and 4 down
+}
