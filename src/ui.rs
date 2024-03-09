0ed6624d58b8891f3f86fee08d18f36722026a99
Kevin Müller <mkevin2006@gmail.com>
2024-03-09 15:29:00 +0100

    Add module for task ui

--- src/ui.rs
+++ src/ui.rs
@@ -1,7 +1,8 @@
 use bevy::prelude::*;
 
-pub mod ui_timer;
 pub mod ui_cursor;
+pub mod ui_timer;
+pub mod ui_tasks;
 
 pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(NodeBundle {
