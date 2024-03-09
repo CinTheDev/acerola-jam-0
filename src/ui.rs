ac597a0d713f4a0071da5ac5641ade963dcb6e8a
Kevin Müller <mkevin2006@gmail.com>
2024-03-09 13:54:16 +0100

    Spawn default cursor image

--- src/ui.rs
+++ src/ui.rs
@@ -3,7 +3,7 @@ use bevy::prelude::*;
 pub mod ui_timer;
 pub mod ui_cursor;
 
-pub fn setup(mut commands: Commands) {
+pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(NodeBundle {
         style: Style {
             width: Val::Percent(100.0),
@@ -14,6 +14,6 @@ pub fn setup(mut commands: Commands) {
         ..default()
     }).with_children(|parent| {
         ui_timer::spawn_ui(parent);
-        ui_cursor::spawn_ui(parent);
+        ui_cursor::spawn_ui(parent, asset_server);
     });
 }
--- src/ui/ui_cursor.rs
+++ src/ui/ui_cursor.rs
@@ -1,5 +1,19 @@
 use bevy::prelude::*;
 
-pub fn spawn_ui(parent: &mut ChildBuilder) {
-    todo!();
+#[derive(Component)]
+struct Cursor;
+
+pub fn spawn_ui(parent: &mut ChildBuilder, asset_server: Res<AssetServer>) {
+    parent.spawn((
+        ImageBundle {
+            style: Style {
+                width: Val::Px(32.0),
+                height: Val::Px(32.0),
+                ..default()
+            },
+            image: UiImage::new(asset_server.load("img/cursor_default.png")),
+            ..default()
+        },
+        Cursor,
+    ));
 }
