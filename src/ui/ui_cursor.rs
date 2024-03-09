39005374ed1cc5a7ff20df866dc0310c152b959d
Kevin Müller <mkevin2006@gmail.com>
2024-03-09 14:42:27 +0100

    Add select cursor

--- src/ui/ui_cursor.rs
+++ src/ui/ui_cursor.rs
@@ -1,9 +1,13 @@
 use bevy::prelude::*;
 
 #[derive(Component)]
-struct Cursor;
+struct CursorDefault;
+
+#[derive(Component)]
+struct CursorSelect;
 
 pub fn spawn_ui(parent: &mut ChildBuilder, asset_server: Res<AssetServer>) {
+    // Default cursor
     parent.spawn(NodeBundle {
         style: Style {
             width: Val::Percent(100.0),
@@ -26,7 +30,34 @@ pub fn spawn_ui(parent: &mut ChildBuilder, asset_server: Res<AssetServer>) {
                 image: UiImage::new(asset_server.load("img/cursor_default.png")),
                 ..default()
             },
-            Cursor,
+            CursorDefault,
+        ));
+    });
+
+    // Select cursor
+    parent.spawn(NodeBundle {
+        style: Style {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            display: Display::None,
+            position_type: PositionType::Absolute,
+            align_items: AlignItems::Center,
+            justify_items: JustifyItems::Center,
+            ..default()
+        },
+        ..default()
+    }).with_children(|root| {
+        root.spawn((
+            ImageBundle {
+                style: Style {
+                    width: Val::Px(8.0),
+                    height: Val::Px(8.0),
+                    ..default()
+                },
+                image: UiImage::new(asset_server.load("img/cursor_select.png")),
+                ..default()
+            },
+            CursorSelect,
         ));
     });
 }
