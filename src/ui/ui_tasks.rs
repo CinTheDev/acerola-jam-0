d8547deeeec25b1a30636ad56c4d20f71c679085
Kevin Müller <mkevin2006@gmail.com>
2024-03-09 15:42:32 +0100

    Add function for adding task text

--- src/ui/ui_tasks.rs
+++ src/ui/ui_tasks.rs
@@ -13,10 +13,27 @@ pub fn spawn_ui(parent: &mut ChildBuilder) {
             style: Style {
                 width: Val::Px(400.0),
                 height: Val::Px(300.0),
+                margin: UiRect::all(Val::Px(5.0)),
+                border: UiRect::all(Val::Px(5.0)),
+                display: Display::Grid,
                 ..default()
             },
             background_color: Color::rgb(0.0, 0.0, 0.0).into(),
             ..default()
         },
-    ));
+    )).with_children(|task_root| {
+        spawn_task_text(task_root);
+    });
+}
+
+fn spawn_task_text(parent: &mut ChildBuilder) {
+    parent.spawn(
+        TextBundle::from_section(
+            "Test",
+            TextStyle {
+                font_size: 18.0,
+                ..default()
+            }
+        )
+    );
 }
