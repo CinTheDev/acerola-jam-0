902cc185931088e7ef1a24a48254d105d7ad808f
Kevin Müller <mkevin2006@gmail.com>
2024-03-09 12:55:51 +0100

    Draw some UI rect at beginning

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ fn main() {
             generate_colliders::generate_colliders,
             player::tasks::instance_tasks,
             timer::setup_losetimer,
+            ui::setup,
         ))
         .add_systems(Update, (
             player::move_player,
--- src/ui.rs
+++ src/ui.rs
@@ -1,1 +1,25 @@
 use bevy::prelude::*;
+
+pub fn setup(mut commands: Commands) {
+    commands.spawn(NodeBundle {
+        style: Style {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            justify_content: JustifyContent::SpaceBetween,
+            ..default()
+        },
+        ..default()
+    }).with_children(|parent| {
+        // Timer
+        parent.spawn(NodeBundle {
+            style: Style {
+                width: Val::Px(200.0),
+                height: Val::Px(100.0),
+                border: UiRect::all(Val::Px(2.0)),
+                ..default()
+            },
+            background_color: Color::rgb(0.0, 0.0, 0.0).into(),
+            ..default()
+        });
+    });
+}
