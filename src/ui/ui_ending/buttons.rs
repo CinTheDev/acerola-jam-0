2107d1a31b48ee715e82d276eea559a424012f64
Kevin Müller <mkevin2006@gmail.com>
2024-03-10 15:00:12 +0100

    Define background for button

--- src/ui/ui_ending/buttons.rs
+++ src/ui/ui_ending/buttons.rs
@@ -1,3 +1,34 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct RestartButton;
 
+#[derive(Component)]
+struct QuitButton;
+
+pub fn spawn_ui(parent: &mut ChildBuilder) {
+    parent.spawn((
+        get_button_background(Color::rgb(0.0, 1.0, 0.0)),
+        RestartButton,
+    ));
+
+    parent.spawn((
+        get_button_background(Color::rgb(1.0, 0.0, 0.0)),
+        QuitButton,
+    ));
+}
+
+fn get_button_background(bg: Color) -> ButtonBundle {
+    ButtonBundle {
+        style: Style {
+            width: Val::Px(300.0),
+            height: Val::Px(200.0),
+            border: UiRect::all(Val::Px(5.0)),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            ..default()
+        },
+        background_color: bg.into(),
+        ..default()
+    }
+}
