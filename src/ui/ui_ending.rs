21ac4a0ea1c30eb479ae035ef0fac0b2da4d184f
Kevin Müller <mkevin2006@gmail.com>
2024-03-10 13:03:10 +0100

    Implement hide_ui

--- src/ui/ui_ending.rs
+++ src/ui/ui_ending.rs
@@ -1,3 +1,16 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+pub struct EndingUI;
 
+pub fn spawn_ui(parent: &mut ChildBuilder) {
+
+}
+
+pub fn hide_ui(
+    mut query: Query<&mut Style, With<EndingUI>>,
+) {
+    for mut style in query.iter_mut() {
+        style.display = Display::None;
+    }
+}
