26f646b2df8a202023606e713bc4276b66c0e5b3
Kevin Müller <mkevin2006@gmail.com>
2024-03-10 13:56:39 +0100

    Add simple text for good ending

--- src/ui/ui_ending/good_ending.rs
+++ src/ui/ui_ending/good_ending.rs
@@ -1,8 +1,15 @@
 use bevy::prelude::*;
 
 const ENDING_TEXT: &str = "
-This is some test text
-Yeah
+You did it!
+
+By redoing the experiment that started all of this in the first place,
+you effectively reverted all modifications of physics, which definetly
+would've destroyed all life on earth otherwise.
+
+However, the false Vauum Decay is still expanding (even if it's 'hollow' now),
+Earth won't be affected, but some unlucky stars might get obliterated if they
+enter the false vacuum at the same time as other big celestial objects.
 ";
 
 pub fn spawn_ui(parent: &mut ChildBuilder) {
