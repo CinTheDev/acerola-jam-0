a966178df15ccd472bf53c2ee4d73cfd24550bff
Kevin Müller <mkevin2006@gmail.com>
2024-03-10 14:02:10 +0100

    Add padding to good_ending text as well

--- src/ui/ui_ending/good_ending.rs
+++ src/ui/ui_ending/good_ending.rs
@@ -1,16 +1,15 @@
 use bevy::prelude::*;
 
-const ENDING_TEXT: &str = "
-You did it!
+const ENDING_TEXT: &str = 
+"You did it!
 
-By redoing the experiment that started all of this in the first place,
-you effectively reverted all modifications of physics, which definetly
+By redoing the experiment that started all of this in the first place, \
+you effectively reverted all modifications of physics, which definetly \
 would've destroyed all life on earth otherwise.
 
-However, the false Vauum Decay is still expanding (even if it's 'hollow' now),
-Earth won't be affected, but some unlucky stars might get obliterated if they
-enter the false vacuum at the same time as other big celestial objects.
-";
+However, the false Vauum Decay is still expanding (even if it's 'hollow' now), \
+Earth won't be affected, but some unlucky stars might get obliterated if they \
+enter the false vacuum at the same time as other big celestial objects.";
 
 pub fn spawn_ui(parent: &mut ChildBuilder) {
     parent.spawn(TextBundle::from_section(
