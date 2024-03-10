1e3260c2bc55d1cfc58f86c32b8b5dfeb4ac35e4
Kevin Müller <mkevin2006@gmail.com>
2024-03-10 15:02:35 +0100

    Define text for button

--- src/ui/ui_ending/buttons.rs
+++ src/ui/ui_ending/buttons.rs
@@ -10,12 +10,16 @@ pub fn spawn_ui(parent: &mut ChildBuilder) {
     parent.spawn((
         get_button_background(Color::rgb(0.0, 1.0, 0.0)),
         RestartButton,
-    ));
+    )).with_children(|bg| {
+        bg.spawn(get_button_text("Restart"));
+    });
 
     parent.spawn((
         get_button_background(Color::rgb(1.0, 0.0, 0.0)),
         QuitButton,
-    ));
+    )).with_children(|bg| {
+        bg.spawn(get_button_text("Quit"));
+    });
 }
 
 fn get_button_background(bg: Color) -> ButtonBundle {
@@ -31,4 +35,15 @@ fn get_button_background(bg: Color) -> ButtonBundle {
         background_color: bg.into(),
         ..default()
     }
+}
+
+fn get_button_text(text: &str) -> TextBundle {
+    TextBundle::from_section(
+        text,
+        TextStyle {
+            font_size: 30.0,
+            color: Color::rgb(1.0, 1.0, 1.0),
+            ..default()
+        }
+    )
 }
