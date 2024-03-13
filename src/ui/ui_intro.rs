c8f5cddb3c2f6fae97789a6a84e785c57694a052
Kevin Müller <mkevin2006@gmail.com>
2024-03-13 20:29:08 +0100

    Add component to manage slide sliding

--- src/ui/ui_intro.rs
+++ src/ui/ui_intro.rs
@@ -1,5 +1,13 @@
 use bevy::prelude::*;
 
+const LERP_FACTOR: f32 = 0.5;
+
+#[derive(Component)]
+pub struct IntroSlide {
+    id: u8,
+    dist_to_top: f32,
+}
+
 pub fn spawn_ui(parent: &mut ChildBuilder) {
     parent.spawn(NodeBundle {
         style: Style {
@@ -11,23 +19,29 @@ pub fn spawn_ui(parent: &mut ChildBuilder) {
         background_color: Color::BLACK.into(),
         ..default()
     }).with_children(|bg| {
-        spawn_slide(bg);
+        spawn_slide(bg, 0, "Hi what's up");
     });
 }
 
-fn spawn_slide(parent: &mut ChildBuilder) {
-    parent.spawn(NodeBundle {
-        style: Style {
-            width: Val::Percent(100.0),
-            height: Val::Percent(100.0),
-            position_type: PositionType::Absolute,
-            padding: UiRect::all(Val::Percent(10.0)),
+fn spawn_slide(parent: &mut ChildBuilder, id: u8, text: &str) {
+    parent.spawn((
+        NodeBundle {
+            style: Style {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                position_type: PositionType::Absolute,
+                padding: UiRect::all(Val::Percent(10.0)),
+                ..default()
+            },
             ..default()
         },
-        ..default()
-    }).with_children(|bg| {
+        IntroSlide {
+            id,
+            dist_to_top: 100.0 * id as f32,
+        }
+    )).with_children(|bg| {
         bg.spawn(TextBundle::from_section(
-            "Hi what's up",
+            text,
             TextStyle {
                 font_size: 32.0,
                 color: Color::WHITE,
