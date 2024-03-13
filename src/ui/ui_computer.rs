e1b7e263c6219382ea9cbd0932d9c5e790c3e782
Kevin Müller <mkevin2006@gmail.com>
2024-03-13 17:42:17 +0100

    Adjust background color and position

--- src/ui/ui_computer.rs
+++ src/ui/ui_computer.rs
@@ -9,13 +9,13 @@ pub fn spawn_ui(parent: &mut ChildBuilder) {
     parent.spawn(
         NodeBundle {
             style: Style {
-                width: Val::Percent(80.0),
-                height: Val::Percent(80.0),
+                width: Val::Percent(70.0),
+                height: Val::Percent(70.0),
                 position_type: PositionType::Absolute,
                 margin: UiRect::all(Val::Auto),
                 ..default()
             },
-            background_color: Color::rgb(1.0, 1.0, 1.0).into(),
+            background_color: Color::BEIGE.into(),
             ..default()
         }
     ).with_children(|bg| {
