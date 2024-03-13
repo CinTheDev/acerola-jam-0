57da0117266109f797ff6e3be581f48f6bf47af2
Kevin Müller <mkevin2006@gmail.com>
2024-03-13 20:24:32 +0100

    Add function to spawn intro slides

--- src/ui/ui_intro.rs
+++ src/ui/ui_intro.rs
@@ -10,5 +10,29 @@ pub fn spawn_ui(parent: &mut ChildBuilder) {
         },
         background_color: Color::BLACK.into(),
         ..default()
+    }).with_children(|bg| {
+        spawn_slide(bg);
+    });
+}
+
+fn spawn_slide(parent: &mut ChildBuilder) {
+    parent.spawn(NodeBundle {
+        style: Style {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            position_type: PositionType::Absolute,
+            padding: UiRect::all(Val::Percent(10.0)),
+            ..default()
+        },
+        ..default()
+    }).with_children(|bg| {
+        bg.spawn(TextBundle::from_section(
+            "Hi what's up",
+            TextStyle {
+                font_size: 32.0,
+                color: Color::WHITE,
+                ..default()
+            }
+        ));
     });
 }
